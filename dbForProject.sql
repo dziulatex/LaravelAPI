82a2abdf6855766a870dd3f3b6e11bfbb370cc08
dziulatex <38464243+dziulatex@users.noreply.github.com>
2021-09-18 22:34:24 +0200

    Add files via upload
    teraz sie skaplem ze heidi sql domyslnie importuje cala baze bez tabel:)

--- dbForProject.sql
+++ dbForProject.sql
@@ -12,13 +12,132 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Eksport danych został odznaczony.
+
+-- Zrzut struktury bazy danych laravelapi
+CREATE DATABASE IF NOT EXISTS `laravelapi` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `laravelapi`;
+
+-- Zrzut struktury tabela laravelapi.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Eksport danych został odznaczony.
 
--- Eksport danych został odznaczony.
+-- Zrzut struktury tabela laravelapi.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.posts_of_users
+CREATE TABLE IF NOT EXISTS `posts_of_users` (
+  `id` int(10) unsigned NOT NULL,
+  `userId` int(10) unsigned DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `body` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Indeks 2` (`userId`),
+  CONSTRAINT `FK_posts_of_users_users_basic_info` FOREIGN KEY (`userId`) REFERENCES `users_basic_info` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.users_additional_info
+CREATE TABLE IF NOT EXISTS `users_additional_info` (
+  `userId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `street` varchar(20) DEFAULT NULL,
+  `suite` varchar(15) DEFAULT NULL,
+  `city` varchar(25) DEFAULT NULL,
+  `zipcode` varchar(10) DEFAULT NULL,
+  `latitude` float(8,4) DEFAULT NULL,
+  `longtitude` float(8,4) DEFAULT NULL,
+  `website` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`userId`) USING BTREE,
+  CONSTRAINT `fk userId` FOREIGN KEY (`userId`) REFERENCES `users_basic_info` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.users_basic_info
+CREATE TABLE IF NOT EXISTS `users_basic_info` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+-- Eksport danych został odznaczony.
+
+-- Zrzut struktury tabela laravelapi.users_companies
+CREATE TABLE IF NOT EXISTS `users_companies` (
+  `userId` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `companyName` varchar(50) DEFAULT NULL,
+  `catchPhrase` varchar(70) DEFAULT NULL,
+  `short` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Indeks 1` (`userId`),
+  CONSTRAINT `FK__users_basic_info` FOREIGN KEY (`userId`) REFERENCES `users_basic_info` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
 -- Eksport danych został odznaczony.
 
